2b5b5f8ce8a1ee23187792d3fdb7900fbbe9e8c8
Ethan_Davis <ethdav8406@gmail.com>
2025-09-22 12:15:29 +0100

    test commit

--- SOF1 Problems/firstpractice.py
+++ SOF1 Problems/firstpractice.py
@@ -3,3 +3,11 @@ This is the first practice problems for SOF1
 Author: Ethan Davis
 """
 
+def input_test():
+    user_input = input('What is your name?\n>>> ')
+    print('Hello,', user_input, '!')
+
+def main():
+    input_test()
+
+main()
