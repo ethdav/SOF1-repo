e3b9d00fab8dc28edfa47bc502817727f2e5434a
Ethan_Davis <ethdav8406@gmail.com>
2025-10-04 11:32:24 +0100

    finished the palindrome exercise, passes intial testing, including the difficult case

--- SOF1 Problems/week 3/palindrome.py
+++ SOF1 Problems/week 3/palindrome.py
@@ -5,19 +5,22 @@ Author: Ethan Davis
 
 def format_string():
     string = str(input("Enter a string: "))
+    string = string.lower()
     string_list = []
     for char in string:
         if char.isalpha():
             string_list.append(char)
     print(string_list)
+    return string_list
 
 def is_palindrome(string_list):
-    for letter in len(string_list)//2: #floor division to get half of the list regardless if odd or even
-        if string_list[letter] == string_list[len(string_list)-letter]:
+    for letter in range(len(string_list)//2): #floor division to get half of the list regardless if odd or even
+        if string_list[letter] == string_list[len(string_list)-1-letter]:
             continue
         else:
-            print("The given word is not a palindrome")
-            break
+            print("The given string is not a palindrome")
+            return
+    print("The given string is a palindrome")
 
 def main():
     string_list = format_string()
