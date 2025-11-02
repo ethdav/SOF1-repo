15f0d600bc1a52f81b10668dd32de85cd49e8be1
Ethan_Davis <ethdav8406@gmail.com>
2025-11-02 18:47:53 +0000

    exercise 3 for the lab done

--- SOF1 Problems/week_6/labExercises.py
+++ SOF1 Problems/week_6/labExercises.py
@@ -26,9 +26,23 @@ def save_list_to_file(sentences, filename):
         file.write(item + "\n")
     file.close()
 
+def save_to_log(entry, logfile):
+    """Appends the entry to the end of the log
+
+    Args:
+        entry (_type_): _description_
+        logfile (_type_): _description_
+    """
+    log = open(logfile,"a")
+    log.write("\n" + entry)
+    log.close()
+
 def main():
-    sentences = ["This","is","a","sentence"]
-    filename = "./week_6/exo2.txt"
-    save_list_to_file(sentences,filename)
+    #sentences = ["This","is","a","sentence"]
+    #filename = "./week_6/exo2.txt"
+    #save_list_to_file(sentences,filename)
+    entry = "newest entry number 3"
+    logfile = "./week_6/log.txt"
+    save_to_log(entry, logfile)
 
 main()
