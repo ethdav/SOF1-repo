09166ed835b04d7dd3ab7bcd1df303d939b23bce
Ethan_Davis <ethdav8406@gmail.com>
2025-11-24 11:18:10 +0000

    first attempt at a solution for the second seminar exercise, I want to try a different one

--- SOF1 Problems/week_9/seminar2.py
+++ SOF1 Problems/week_9/seminar2.py
@@ -9,5 +9,13 @@ def max_cheese(case_allowance, *cheeses):
     Args:
         case_allowance (_type_): _description_
     """
-    pricePerGram = []
+    pricePerGram = sorted([(x[1]/x[0]) for x in cheeses],reverse=True)
     for cheese in cheeses:
+        remainder = None
+
+def main():
+    cheese1, cheese2, cheese3 = (3000,150), (2000,80), (3000,190)
+    print(max_cheese(6000, cheese1, cheese2, cheese3))
+
+if __name__ == "__main__":
+    main()
