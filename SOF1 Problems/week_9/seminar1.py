54e01b97001ec9c6ced8cef3f441bdc0e02bb62b
Ethan_Davis <ethdav8406@gmail.com>
2025-11-25 16:41:40 +0000

    Made a recursive version of the coin problem for week 9's seminar

--- SOF1 Problems/week_9/seminar1.py
+++ SOF1 Problems/week_9/seminar1.py
@@ -4,7 +4,7 @@ Author: Ethan Davis
 """
 
 # We are gonna do maths with whole numbers and then divide the result by 100
-DENOMINATIONS = [200, 100, 50, 20, 10, 5, 2, 1]
+DENOMS = [200, 100, 50, 20, 10, 5, 2, 1]
 
 def min_coins(total):
     """Finds the minimum number of coins required to sum to the given
@@ -18,15 +18,30 @@ def min_coins(total):
         raise TypeError(f"Invalid type for total:{type(total)}")
     else:
         shifted = total * 100
-        coin_list = {("£"+str(x/100)):0 for x in DENOMINATIONS}
-        for denom in DENOMINATIONS:
-            coin_list["£"+str(denom/100)] = shifted // denom
+        coin_list = {("£"+str(x/100)):0 for x in DENOMS}
+        for denom in DENOMS:
+            coin_list["£"+str(denom/100)] = int(shifted // denom)
             shifted = shifted % denom
         return coin_list
     
+def min_coins_recursive(total, id=0, coin_dict={("£"+str(x/100)):0 for x in DENOMS}):
+    """Finds the minimum number of coins required to sum to the given
+    total recursively
+
+    Args:
+        total (_type_): _description_
+    """
+    if total == 0 or id == len(DENOMS):
+        return coin_dict
+    else:
+        coin_dict["£"+str(DENOMS[id]/100)] = int(total // (DENOMS[id]/100))
+        total = total % (DENOMS[id]/100)
+        id+=1
+        return min_coins_recursive(total, id, coin_dict)
+
 def main():
-    total = 143.94
-    print(min_coins(total))
+    total = 19.51
+    print(min_coins_recursive(total))
 
 if __name__ == "__main__":
     main()
