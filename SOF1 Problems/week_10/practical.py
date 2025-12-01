9ce0505d9083d1a1f79f3c238f0c0267d62b64e4
Ethan_Davis <ethdav8406@gmail.com>
2025-12-01 11:41:10 +0000

    partial implementation of the first exercise for week 10's practical. Methods include push, pop, __str__, and __len__.

--- SOF1 Problems/week_10/practical.py
+++ SOF1 Problems/week_10/practical.py
@@ -7,4 +7,44 @@ from linkednode import LinkedNode
 
 class LinkedStack:
     def __init__(self):
-        pass
+        self._top = None
+        self._size = 0
+
+    def __str__(self):
+        if self._top is None:
+            return "Stack([])"
+        else:
+            return "Stack([" + str(self._top) + "])"
+        
+    def __len__(self):
+        return self._size
+        
+    def push(self, value):
+        if self._top is None:
+            self._top = LinkedNode(value)
+        else:
+            new_top = LinkedNode(value)
+            new_top._next = self._top
+            self._top = new_top
+        self._size += 1
+    
+    def pop(self):
+        if self._size == 0:
+            raise ValueError("Stack is empty")
+        else:
+            popped = self._top
+            self._top = popped._next
+            popped._next = None
+            self._size -= 1
+            return popped
+        
+def main():
+    stacky = LinkedStack()
+    stacky.push("first")
+    stacky.push("second")
+    print(stacky)
+    print(stacky.pop())
+    print(stacky)
+
+if __name__ == "__main__":
+    main()
