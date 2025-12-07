3a14b580a59ae8ef814c1042001d1186a606c1c2
Ethan_Davis <ethdav8406@gmail.com>
2025-12-07 15:21:34 +0000

    Finished question 3 from formative 2, all seems to work as requested. Time to completion: 17:15

---
+++ SOF1-EvenYears-Formative2-sourcediles/question_3.py
@@ -0,0 +1,30 @@
+"""
+Answer to question 3 of Formative 2
+"""
+
+def read_from_file(filename):
+    """Reads a file for the values of an athletes times
+
+    Args:
+        filename (_type_): _description_
+    """
+    file = open(filename)
+    athlete_dict = dict()
+    try:
+        for line in file:
+            if line[0] == "#":
+                pass
+            else:
+                split = line.strip().split(",")
+                times_dict = {"200m":float(split[1]),"110m":float(split[2]),"800m":float(split[3])}
+                athlete_dict[split[0]] = times_dict
+        return athlete_dict
+    except: 
+        raise ValueError("File is invalid format")
+
+def main():
+    filename = "./data/dataWithoutComments.txt"
+    print(read_from_file(filename))
+
+if __name__ == "__main__":
+    main()
